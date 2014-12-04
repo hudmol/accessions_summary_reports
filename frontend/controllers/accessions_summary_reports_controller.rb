f7639717da71aa07dbbb51c2846e8b554bfa36ed
jambun <jambun@gmail.com>
2014-12-05 09:48:56 +1100

    first cut frontend and schema

--- frontend/controllers/accessions_summary_reports_controller.rb
+++ frontend/controllers/accessions_summary_reports_controller.rb
@@ -3,7 +3,12 @@ class AccessionsSummaryReportsController < ApplicationController
   skip_before_filter :unauthorised_access
 
   def index
+    @accessions_summary_report = JSONModel(:accessions_summary_report).new._always_valid!
   end
    
+  def create
+
+  end
+
 end
 
---
+++ schemas/accessions_summary_report.rb
@@ -0,0 +1,17 @@
+{
+  :schema => {
+    "$schema" => "http://www.archivesspace.org/archivesspace.json",
+    "version" => 1,
+    "type" => "object",
+    "uri" => "/repositories/:repo_id/accessions_summary_reports",
+    "properties" => {
+      "uri" => {"type" => "string", "required" => false},
+
+      "start_date" => {"type" => "date", "minLength" => 1, "ifmissing" => "error"},
+      "end_date" => {"type" => "date", "minLength" => 1, "ifmissing" => "error"},
+
+      "report" => {"type" => "string", "enum" => ["received", "processed"], "ifmissing" => "error"},
+
+    },
+  },
+}
