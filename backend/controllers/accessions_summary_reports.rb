1a1ce1431224ac4d5dfa6663ed8e6c024e4eeb49
jambun <jambun@gmail.com>
2014-12-05 20:58:34 +1100

    backend for received report

--- backend/controllers/accessions_summary_reports.rb
+++ backend/controllers/accessions_summary_reports.rb
@@ -9,16 +9,78 @@ class ArchivesSpaceService < Sinatra::Base
     .permissions([])
     .returns([200, "Report data"]) \
   do
+
     data = {}
 
-    DB.open do |db|
-      ds = db[:accession].filter(:repo_id => params[:repo_id]).
-        where('accession_date >= ? AND accession_date <= ?', params[:start_date], params[:end_date])
-
-      data["count"] = ds.count
+    if params[:report] == 'received'
+      data = run_received_report(params)
+    elsif params[:report] == 'processed'
+      data = run_processed_report(params)
     end
 
     json_response(data)
+
+  end
+
+  private
+
+  def run_received_report(params)
+    data = {}
+    DB.open do |db|
+      ds = db[:accession]
+        .select(:accession__id, :accession__identifier, :user_defined__boolean_1, :extent__number)
+        .left_outer_join(:user_defined, {:accession_id => :accession__id}, :table_alias => :user_defined)
+        .left_outer_join(:extent, {:accession_id => :accession__id}, :table_alias => :extent)
+        .filter(:repo_id => params[:repo_id])
+        .where('accession_date >= ? AND accession_date <= ?', params[:start_date], params[:end_date])
+
+      data[:total_accs] = 0
+      data[:new_accs] = 0
+      data[:add_accs] = 0
+      data[:total_extent] = 0.0
+      data[:new_extent] = 0.0
+      data[:add_extent] = 0.0
+      
+      ds.each do |row|
+        data[:total_accs] += 1
+        data[:total_extent] += row[:number].to_f
+        if row[:boolean_1] == 1
+          data[:new_accs] += 1
+          data[:new_extent] += row[:number].to_f
+        else
+          data[:add_accs] += 1
+          data[:add_extent] += row[:number].to_f
+        end
+      end
+
+      puts "SSSSSSSSSSSS  #{ds.sql}"
+      puts "DDDDDDDDDDDD  #{ds.all}"
+    end
+    data
+  end
+
+
+  def run_processed_report(params)
+    data = {}
+    DB.open do |db|
+      ds = db[:event_link_rlshp]
+        .join(:accession, :accession__id => :accession_id)
+        .join(:event, :event__id => :event_id)
+        .filter(:repo_id => params[:repo_id])
+        .where('event_type = ? AND timestamp >= ? AND timestamp <= ?', 'processed', params[:start_date], params[:end_date])
+
+      data[:total_accs] = 0
+      data[:total_extent] = 0
+
+      ds.each do |row|
+        data[:total_accs] += 1
+#        data[:total_extent] += row[:number].to_f
+      end
+
+      puts "SSSSSSSSSSSS  #{ds.sql}"
+      puts "DDDDDDDDDDDD  #{ds.all}"
+    end
+    data
   end
 
 end
